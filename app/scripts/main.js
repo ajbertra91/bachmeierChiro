cf3d7a3f7ec41ca89330d5bf73145de0a3f3a782
Adam Bertrand <adam@sportstechinc.com>
2014-09-14 10:32:59 -0400

    adds about page and contact page styles, markup, images, and some navigational JS animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+  var local = window.location.pathname.split("/")[1];
+  console.log('local: ', local);
+  if (local === home) {
+    $('nav a[href^="#' + local + '"]').addClass('home');
+  }
+
+
+  $(".home-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#home").offset().top
+    }, 1000);
+  });
+  $(".services-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#services").offset().top
+    }, 1000);
+  });
+  $(".about-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 1000);
+  });
+  $(".space-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#space").offset().top
+    }, 1000);
+  });
+  $(".contact-btn").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 1000);
+  });
+});
