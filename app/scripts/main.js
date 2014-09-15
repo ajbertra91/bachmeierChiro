19bdc8f5eae4107f920f1f9630bceab339a2c045
Adam Bertrand <adam@sportstechinc.com>
2014-09-14 22:27:13 -0400

    adds spaces markup and styles, and some css transitions to the navigation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,34 +1,51 @@
 $(function() {
-  var local = window.location.pathname.split("/")[1];
-  console.log('local: ', local);
-  if (local === home) {
-    $('nav a[href^="#' + local + '"]').addClass('home');
-  }
 
-
+  var nav = $('.top-bar-section');
+  var navLinks = nav.find('li');
   $(".home-btn").click(function() {
+    nav.removeClass('home services about space contact');
+    navLinks.removeClass('active');
+    nav.find('.home-btn').addClass('active');
+    nav.addClass('home');
     $('html, body').animate({
         scrollTop: $("#home").offset().top
     }, 1000);
   });
   $(".services-btn").click(function() {
+    nav.removeClass('home services about space contact');
+    navLinks.removeClass('active');
+    nav.find('.services-btn').addClass('active');
+    nav.addClass('services');
     $('html, body').animate({
         scrollTop: $("#services").offset().top
     }, 1000);
   });
   $(".about-btn").click(function() {
+    nav.removeClass('home services about space contact');
+    navLinks.removeClass('active');
+    nav.find('.about-btn').addClass('active');
+    nav.addClass('about');
     $('html, body').animate({
         scrollTop: $("#about").offset().top
     }, 1000);
   });
   $(".space-btn").click(function() {
+    nav.removeClass('home services about space contact');
+    navLinks.removeClass('active');
+    nav.find('.space-btn').addClass('active');
+    nav.addClass('space');
     $('html, body').animate({
         scrollTop: $("#space").offset().top
     }, 1000);
   });
   $(".contact-btn").click(function() {
+    nav.removeClass('home services about space contact');
+    navLinks.removeClass('active');
+    nav.find('.contact-btn').addClass('active');
+    nav.addClass('contact');
     $('html, body').animate({
         scrollTop: $("#contact").offset().top
     }, 1000);
   });
+
 });
